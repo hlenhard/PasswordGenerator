a6c9e71638e51e3b924e40cab394761169ec24e5
hlenhard <105365498+hlenhard@users.noreply.github.com>
2022-09-13 12:21:48 +0200

    Commit initial files

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordOneEl = document.getElementById("password-one");
+let passwordTwoEl = document.getElementById("password-two");
+let length = 15;
+let passwordOne = "";
+let passwordTwo = "";
+
+function getRandomPassword() {
+  passwordOne = "";
+  for (let i = 0; i < length; i++) {
+    passwordOne += characters[Math.floor(Math.random() * characters.length)];
+    passwordOneEl.textContent = passwordOne;
+  }
+
+  passwordTwo = "";
+  for (let i = 0; i < length; i++) {
+    passwordTwo += characters[Math.floor(Math.random() * characters.length)];
+    passwordTwoEl.textContent = passwordTwo;
+  }
+  passwordOne.value = str;
+}
+
+function copyPassword() {}
